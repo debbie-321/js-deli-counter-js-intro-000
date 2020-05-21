65199ac9913d03757692861ed1755cefacfdc464
Deborah Nakakande <d.nakakande@hotmail.com>
2020-05-21 15:07:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ function takeANumber(line, name){
   line.push(name);
   return(`Welcome, ${name}. You are number ${line.indexOf(name) + 1} in line.`)
 }
+
+function nowServing(katzDeliLine){
+  if (katzDeliLine.length === 0){
+    return `There is nobody waiting to be served!`
+  } else {
+    return `Currently serving ${line.shift()}.`
+  }
+}
